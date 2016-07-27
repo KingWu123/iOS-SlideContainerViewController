b0ed205aafb02902529e7b8e3f871b1c23fd73cd
king.wu <iceworldwgq@163.com>
2016-07-27 20:20:38 +0800

    Initial Commit

---
+++ ContainerViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContainerViewController
+//
+//  Created by king.wu on 7/27/16.
+//  Copyright © 2016 king.wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ContainerViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContainerViewController
+//
+//  Created by king.wu on 7/27/16.
+//  Copyright © 2016 king.wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
